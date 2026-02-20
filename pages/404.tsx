23445fb257e17a8a42ae48c220d566e7aa1a1ec9
sorayyaa1 <sorayyaa1@gmail.com>
2026-02-20 21:35:13 +0330

    feat: add custom 404 page, search component, and intro section component for Homepage

--- pages/404.tsx
+++ pages/404.tsx
@@ -16,7 +16,7 @@ function NotFound(){
                alt="404-error-pic"
                height={430}
                width={651}/>
-               <p className="text-[#0C0C0C]">صفحه‌ی  مورد نظر یافت نشد!</p>
+               <p className="text-[#0C0C0C] font-semibold">صفحه‌ی  مورد نظر یافت نشد!</p>
                <p className="text-[#7E8288]">این صفحه در رنتی‌فای یافت نشد. لطفا به صفحه اصلی مراجعه کنید تا املاک مورد نظر خود را پیدا کنید</p>
                <button onClick={backToHomepage} className="px-6 py-3 rounded-lg bg-[#0D6EFD] text-[#FFFFFF]">بازگشت به صفحه اصلی</button>
             </div>
