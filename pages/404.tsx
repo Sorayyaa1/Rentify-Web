5340dca02451ff2e1cd60dc19595b5d973dd4969
sorayyaa1 <sorayyaa1@gmail.com>
2026-02-20 21:25:56 +0330

    feat: add custom 404 page, search component, and intro

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image"
+import { useRouter } from "next/router"
 
 function NotFound(){
+    const router=useRouter()
+
+    function backToHomepage(){
+       router.push('/')
+    }
+
     return(
         <div className="bg-gray-100 py-10">
             <div className="w-10/12 mx-auto h-screen flex flex-col gap-2 items-center bg-[#FFFFFF] rounded-lg">
@@ -11,7 +18,7 @@ function NotFound(){
                width={651}/>
                <p className="text-[#0C0C0C]">صفحه‌ی  مورد نظر یافت نشد!</p>
                <p className="text-[#7E8288]">این صفحه در رنتی‌فای یافت نشد. لطفا به صفحه اصلی مراجعه کنید تا املاک مورد نظر خود را پیدا کنید</p>
-               <button className="px-6 py-3 rounded-lg bg-[#0D6EFD] text-[#FFFFFF]">بازگشت به صفحه اصلی</button>
+               <button onClick={backToHomepage} className="px-6 py-3 rounded-lg bg-[#0D6EFD] text-[#FFFFFF]">بازگشت به صفحه اصلی</button>
             </div>
         </div>
     )
