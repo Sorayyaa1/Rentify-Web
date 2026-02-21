c010dca39d078e217b8d0b1f994bf96efb72247e
sorayyaa1 <sorayyaa1@gmail.com>
2026-02-21 03:39:08 +0330

    style: update search component styles

--- Components/SearchComponent.tsx
+++ Components/SearchComponent.tsx
@@ -1,34 +1,81 @@
+import { Icon } from "@/pages/Components/UI/Icon/Icon";
+// import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@heroui/react";
+
 
 interface selectListType{
     name:string
     id:string
-    option:Array<string>
+    option:Array<optionType>
+}
+interface optionType{
+    title:string
+    value:string
 }
 
 const selectList:selectListType[]=[
-    {name:"نوع قرارداد",id:"contract",option:["رهن","اجاره"]},
-    {name:'نوع ملک',id:"propertyType",option:["آپارتمان","ویلا","خانه ویلایی"]},
-    {name:"موقعیت مکانی",id:"Location",option:["تهران- نیاوران"]}
+    {   name:"موقعیت مکانی",
+        id:"Location",
+        option:[{title:"تهران- نیاوران",value:"tehran-niavaran"}]
+    },
+    {   name:'نوع ملک',
+        id:"propertyType",
+        option:[{title:"آپارتمان",value:"apartment"},{title:"ویلا",value:"villa"},{title:"خانه ویلایی",value:"villaHouse"}]
+    },
+    {   name:"نوع قرارداد",
+        id:"contract",
+        option:[{title:"رهن",value:"mortgage"},{title:"اجاره",value:"rent"}]
+    }
 ]
 
 function SearchComponent(){
    
     return(
-        <form action="" className='flex gap-4'>
-           {/* {
-           selectList.map(({item}:any,{index}:any)=>(
-            <div>
-                <label htmlFor={item.id}>{item.name}</label>
-                <select id={item.id} key={index}>
+        <form action="" className='flex gap-4 w-7/12 absolute z-30 top-44 items-center justify-between bg-[#FFFFFF] rounded-full'>
+           {
+           selectList.map((item,index)=>(
+            <div className="flex flex-col gap-2 py-4 ps-12">
+                <div className="flex gap-2">
+                    <label htmlFor={item.id} className="text-[#73767C]">{item.name}</label>
+                    <Icon icon={"arrowDown"}/>
+                </div>
+                
+                <select id={item.id} key={index} className="text-[#73767C]">
                     {
-                        item.option.map(({item}:any,{index}:any)=>(
-                            <option key={index} value={item.id}>{item}</option>
+                        item.option.map((item,index)=>(
+                            <option key={index} value={item.value} className="text-black">{item.title}</option>
                         ))
                     }
                 </select>
             </div>
            ))
+           }
+           {/* {
+            <Dropdown>
+                {
+                    selectList.map((item,index)=>(
+                       <div>
+                            <DropdownTrigger>
+                               <Button variant="bordered" value={item.id}>{item.name}</Button>
+                            </DropdownTrigger>
+                            <DropdownMenu>
+                                {
+                                    item.option.map((item,index)=>(
+                                        <DropdownItem key={index} value={item.value}>
+                                           {item.title}
+                                       </DropdownItem>
+                                    ))
+                                }
+                              
+                           </DropdownMenu>
+                       </div>
+                    ))
+                }
+            </Dropdown>
            } */}
+           <button className="flex gap-2 py-8 px-6 bg-[#0D6EFD] text-[#FFFFFF] rounded-e-full cursor-pointer">
+            <Icon icon={'magnifyingGlass'} />
+            <p>جستجو</p>
+           </button>
         </form>
     )
 }
--- pages/Components/UI/Icon/Icon.tsx
+++ pages/Components/UI/Icon/Icon.tsx
@@ -1,10 +1,15 @@
-// export function Icon({icon,...props}){
-//     const {icon}=props
-//     switch(icon){
-//         case"arrowDown":
-//             return  <svg {...props} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12 12.3359L9.20706 9.54297L7.79285 10.9572L11.2928 14.4572C11.6834 14.8477 12.3165 14.8477 12.7071 14.4572L16.2071 10.9572L14.7928 9.54297L12 12.3359Z" fill="black"/>
-//                     </svg>
+export function Icon(props:any){
+    const {icon}=props
+    switch(icon){
+        case"arrowDown":
+            return  <svg {...props} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                       <path fill-rule="evenodd" clip-rule="evenodd" d="M12 12.3359L9.20706 9.54297L7.79285 10.9572L11.2928 14.4572C11.6834 14.8477 12.3165 14.8477 12.7071 14.4572L16.2071 10.9572L14.7928 9.54297L12 12.3359Z" fill="#73767C"/>
+                    </svg>
+        case"magnifyingGlass":
+            return  <svg {...props} width="24" height="24" stroke="white" stroke-width="1.7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                       <path fill-rule="evenodd" clip-rule="evenodd" d="M2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10ZM10 6.5C11.933 6.5 13.5 8.067 13.5 10H15.5C15.5 6.96243 13.0376 4.5 10 4.5V6.5Z" fill="#0D6EFD"/>
+                       <path fill-rule="evenodd" clip-rule="evenodd" d="M15.7072 14.293L21.7072 20.293L20.293 21.7072L14.293 15.7072L15.7072 14.293Z" fill="#0D6EFD"/>
+                    </svg>
 
-//     }
-// }
+    }
+}
