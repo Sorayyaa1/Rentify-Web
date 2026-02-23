ce6994af95d5906320a8f49192c4f25d3f2acfe7
sorayyaa1 <sorayyaa1@gmail.com>
2026-02-23 23:01:04 +0330

    feat: update SearchComponent

--- Components/SearchComponent.tsx
+++ Components/SearchComponent.tsx
@@ -30,14 +30,14 @@ const selectList:selectListType[]=[
 function SearchComponent(){
    
     return(
-        <form action="" className='flex gap-4 w-7/12 absolute z-30 top-44 items-center justify-between bg-[#FFFFFF] rounded-full'>
+        <form action="" className='flex gap-4 w-7/12 absolute z-30 top-48 items-center justify-between bg-[#FFFFFF] rounded-full'>
            {
            selectList.map((item,index)=>(
             <div className="flex flex-col gap-2 py-4 ps-12">
-                <div className="flex gap-2">
+                {/* <div className="flex gap-2">
                     <label htmlFor={item.id} className="text-[#73767C]">{item.name}</label>
                     <Icon icon={"arrowDown"}/>
-                </div>
+                </div> */}
                 
                 <select id={item.id} key={index} className="text-[#73767C]">
                     {
